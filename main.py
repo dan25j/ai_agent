eb9d1ed5bcb0dd61a4eb14aa2a219fbdf9526f03
dan25gy <djose102@gmail.com>
2026-03-11 13:49:54 -0600

    Show usage metadata

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ from google import genai
 
 
 def main():
-    print("Hello from ai-agent!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
+
+    if not response.usage_metadata:
+       raise RuntimeError("Gemini API response appears to be malformed")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Total tokens: {response.usage_metadata.total_token_count}")
+    print("Response:")
     print(response.text)
 
 
