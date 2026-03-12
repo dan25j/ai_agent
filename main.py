d191e75ff4ce87ddbfa03144542c344466572925
dan25gy <djose102@gmail.com>
2026-03-12 13:54:43 -0600

    Multiple messages added

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(
+        role="user",
+        parts=[types.Part(text=args.user_prompt)])
+    ]
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model = "gemini-2.5-flash",
+        contents = messages,
     )
 
     if not response.usage_metadata:
