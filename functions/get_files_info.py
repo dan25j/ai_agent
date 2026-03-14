1e5ae136a44653ea39f76bc465526c5764a2c64b
dan25gy <djose102@gmail.com>
2026-03-13 19:31:21 -0600

    Get the contents of a directory

---
+++ functions/get_files_info.py
@@ -0,0 +1,19 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+        valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a valid directory'
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            file_info = f"- {filename}: file_size={os.path.getsize(filepath)} bytes, is_dir={os.path.isdir(filepath)}"
+            files_info.append(file_info)
+        return "\n".join(files_info)
+    except OSError as e:
+        return f'Error: {e}'
---
+++ test_get_files_info.py
@@ -0,0 +1,10 @@
+from functions.get_files_info import get_files_info
+
+def test_get_files_info():
+    test_cases = [("calculator", "."), ("calculator", "pkg"), ("calculator", "/bin"), ("calculator", "../")]
+    for test_case in test_cases:
+        result = get_files_info(*test_case)
+        print(f"Result for '{test_case[1]}' directory:\n {result}")
+    
+if __name__ == "__main__":
+    test_get_files_info()
