f7dfd762d10eeef805f66704ea09de3f50992db2
dan25gy <djose102@gmail.com>
2026-03-21 16:53:56 -0600

    Feedback loop

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+import sys
 import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from config import MAX_ITERS
 from call_function import available_functions, call_function
 
 
@@ -25,44 +27,60 @@ def main():
     generate_content(client, messages, args.verbose)
 
 
-def generate_content(client, messages, verbose):
-    response = client.models.generate_content(
-        model = "gemini-2.5-flash",
-        contents = messages,
-        config = types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
-            # Setting temperature to 0 makes the output more deterministic, which is often desirable for code generation tasks.
-            temperature=0
+def generate_content(client, messages, verbose, max_steps=MAX_ITERS):
+    step = 0
+    while step < max_steps:
+        response = client.models.generate_content(
+            model = "gemini-2.5-flash",
+            contents = messages,
+            config = types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+                # Setting temperature to 0 makes the output more deterministic, which is often desirable for code generation tasks.
+                temperature=0
             )
-    )
-
-    if not response.usage_metadata:
-       raise RuntimeError("Gemini API response appears to be malformed")
-
-    if verbose:
-        print(f"User prompt: {verbose}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print(f"Total tokens: {response.usage_metadata.total_token_count}")
+        )
     
-    function_results = []
-    if response.function_calls:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, verbose)
-            if (
-                not function_call_result.parts
-                or not function_call_result.parts[0].function_response
-                or not function_call_result.parts[0].function_response.response
-            ):
-                raise RuntimeError(f"Empty function response for {function_call.name}")
-            function_results.append(function_call_result.parts[0])
-            if verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print("Response:")
-        print(response.text)
 
+        if not response.usage_metadata:
+            raise RuntimeError("Gemini API response appears to be malformed")
+
+        if verbose:
+            print(f"User prompt: {verbose}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            print(f"Total tokens: {response.usage_metadata.total_token_count}")
+
+        if response.candidates and response.candidates[0].content:
+            messages.append(response.candidates[0].content)
+        else:
+            print("\n[Error] Gemini returned an empty response (likely due to safety filters).")
+            sys.exit(1)
+
+        
+        if response.function_calls:
+            function_responses = []
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, verbose)
+                if (
+                    not function_call_result.parts
+                    or not function_call_result.parts[0].function_response
+                    or not function_call_result.parts[0].function_response.response
+                ):
+                    raise RuntimeError(f"Empty function response for {function_call.name}")
+                function_responses.append(function_call_result.parts[0])
+                if verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+                messages.append(types.Content(role="user", parts=function_responses))
+        else:
+            print("\nFinal response:")
+            print(response.text)
+            break
+        
+        step += 1
+    if step >= max_steps:
+        print(f"\n[Error] Task failed: The agent reached the maximum limit of {max_steps} iterations.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
